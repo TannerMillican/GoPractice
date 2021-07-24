6aeba39af866ff31ff3fc0cc355e50d32abeb0c2
Tanner Millican <tmillican@appddictionstudio.com>
2021-07-24 10:09:37 -0500

    Go practice
    Updated outputs to handle multiple different input scenarios from user

--- practice.go
+++ practice.go
@@ -11,8 +11,24 @@ func main() {
 
 	practice_scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("How many days have you been working with Go?")
+
 	practice_scanner.Scan()
 	time_input, _ := strconv.ParseInt(practice_scanner.Text(), 10, 64)
-	fmt.Printf("Hello world! I've worked with Go for %d days", time_input)
+
+	switch {
+	case time_input < 365:
+		fmt.Printf("Hello world! I've worked with Go for %d days", time_input)
+	case time_input%365 == 0:
+		year_number := time_input%365 == 0
+		if year_number {
+			fmt.Printf("Hello world! I've worked with Go for 1 year!")
+		} else {
+			fmt.Printf("Hello world! I've worked with Go for %d years!!", time_input/365)
+		}
+	case time_input > 365 && time_input < 730:
+		fmt.Printf("Hello world! I've worked with Go for 1 year and %d day(s)!!", time_input%365)
+	default:
+		fmt.Printf("Hello world! I've worked with Go for %d years and %d day(s)!!", time_input/365, time_input%365)
+	}
 
 }
